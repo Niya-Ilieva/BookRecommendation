f600f1245de8ff378079b382f2fc3f2b02585638
Niya-Ilieva <86787390+Niya-Ilieva@users.noreply.github.com>
2022-04-06 18:17:58 +0300

    adding formating constant

---
+++ BookRecommendation.Core/Constants/FormatingConstant.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookRecommendation.Core.Constants
+{
+    public class FormatingConstant
+    {
+        public const string DateFormat = "dd.MM.yyyy";
+    }
+}
